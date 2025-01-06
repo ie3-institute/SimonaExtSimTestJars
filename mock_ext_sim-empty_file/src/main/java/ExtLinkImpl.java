907d5d7b0b7de1de797d12dd8f85fa03aeb55b0b
staudtMarius <marius.staudt@tu-dortmund.de>
2025-01-06 15:29:02 +0100

    Setting up project.

---
+++ mock_ext_sim-empty_file/src/main/java/ExtLinkImpl.java
@@ -0,0 +1,8 @@
+import edu.ie3.simona.api.ExtLinkInterface;
+import edu.ie3.simona.api.simulation.ExtSimAdapterData;
+
+public class ExtLinkImpl implements ExtLinkInterface {
+
+    @Override
+    public void setup(ExtSimAdapterData extSimAdapterData) {}
+}
---
+++ mock_ext_sim-two_implementations/src/main/java/ExtLinkImpl2.java
@@ -0,0 +1,8 @@
+import edu.ie3.simona.api.ExtLinkInterface;
+import edu.ie3.simona.api.simulation.ExtSimAdapterData;
+
+public class ExtLinkImpl2 implements ExtLinkInterface {
+
+    @Override
+    public void setup(ExtSimAdapterData extSimAdapterData) {}
+}
